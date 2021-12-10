b94d1bb68f2ff74c405a3672b20ad94db8a783cd
aarush-narang <aarushnarang@gmail.com>
2021-12-09 19:40:55 -0800

    added sin, cos, and tan functions. also fixed logistic function example.

--- api-backend/src/query-functions/generatePoints.js
+++ api-backend/src/query-functions/generatePoints.js
@@ -7,7 +7,7 @@ const {
 
 class GeneratePoints {
     constructor(minX, maxX, len, spread, eq, type) {
-        if (!(eq instanceof Array)) throw new UserInputError('Equation must be given in an array of coefficients of a polynomial in ascending order of power. The highest power will be automatically interpreted. 0\'s are allowed.')
+        if (!(eq instanceof Array)) throw new UserInputError('Equation must be given in an array of coefficients of a polynomial in ascending order of power. The highest power will be automatically interpreted. put a 0.')
 
         this.spread = spread
         this.len = len
@@ -49,7 +49,7 @@ class GeneratePoints {
         } else if (['log'].includes(this.type)) { // log functions
             // [a, b, c, d, e] => f(x) = a*log_b(cx + d) + e. 0's are used for empty variables.
             const getLogYVal = (eq, x) => {
-                if (eq.length !== 5) throw new UserInputError('Equation was not provided in proper format. [a, b, c, d, e] => f(x) = a*log_b(cx + d) + e. If a number is not applicable, 0\'s are allowed.')
+                if (eq.length !== 5) throw new UserInputError('Equation was not provided in proper format. [a, b, c, d, e] => f(x) = a*log_b(cx + d) + e. If a number is not applicable, put a 0.')
                 if ((eq[2] * x) + eq[3] < 1) throw new UserInputError('Log of numbers less than 1 are not possible.')
                 if (eq[1] < 2) throw new UserInputError('Base cannot be lower than 2')
                 if (eq[0] === 0) throw new UserInputError('"a" cannot be equal to 0, otherwise it is not a log function.')
@@ -62,7 +62,7 @@ class GeneratePoints {
         } else if (['logistic'].includes(this.type)) { // logistic functions
             // [a, b, c, d] => f(x) = a / b + c * e^(d * x). 0's are used for empty variables. In this equation, e is not a variable but is an irrational number appx. equal to 2.718. 
             const getLogisticYVal = (eq, x) => {
-                if (eq.length !== 4) throw new UserInputError('Equation was not provided in proper format. [a, b, c, d] => f(x) = a / b + c * e^(d * x). If a number is not applicable, 0\'s are allowed.')
+                if (eq.length !== 4) throw new UserInputError('Equation was not provided in proper format. [a, b, c, d] => f(x) = a / b + c * e^(d * x). If a number is not applicable, put a 0.')
                 if (eq[0] === 0) throw new UserInputError('"a" cannot be equal to 0 in order to make a logistic function')
                 if (eq[1] < 1) throw new UserInputError('"b" cannot be less than 1 in order to make a logistic function')
                 if (eq[2] < 1) throw new UserInputError('"c" cannot be less than 1 in order to make a logistic function')
@@ -73,6 +73,43 @@ class GeneratePoints {
             for (let xindex = 0; xindex < xvalues.length; xindex++) {
                 yvalues.push(getLogisticYVal(this.eq, xvalues[xindex]) + getAdd(this.spread))
             }
+        } else if (['sin'].includes(this.type)) { // sin functions
+            // [a, b, c, d] => f(x) = a * sin(b * x + c) + d. 0's are used for empty variables.
+            const getSinYVal = (eq, x) => {
+                if (eq.length !== 4) throw new UserInputError('Equation was not provided in proper format. [a, b, c, d] => f(x) = a * sin(b * x + c) + d. If a number is not applicable, put a 0.')
+                if (eq[0] === 0) throw new UserInputError('"a" cannot be equal to 0 in order to make a sin function')
+                if (eq[1] === 0) throw new UserInputError('"b" cannot be equal to 0 in order to make a sin function')
+
+                return (eq[0] * Math.sin(eq[1] * (x + eq[2]))) + eq[3]
+            }
+            for (let xindex = 0; xindex < xvalues.length; xindex++) {
+                yvalues.push(getSinYVal(this.eq, xvalues[xindex]) + getAdd(this.spread))
+            }
+        } else if (['cos'].includes(this.type)) { // cos functions
+            // [a, b, c, d] => f(x) = a * cos(b * x + c) + d. 0's are used for empty variables.
+            const getCosYVal = (eq, x) => {
+                if (eq.length !== 4) throw new UserInputError('Equation was not provided in proper format. [a, b, c, d] => f(x) = a * cos(b * x + c) + d. If a number is not applicable, put a 0.')
+                if (eq[0] === 0) throw new UserInputError('"a" cannot be equal to 0 in order to make a cos function')
+                if (eq[1] === 0) throw new UserInputError('"b" cannot be equal to 0 in order to make a cos function')
+
+                return (eq[0] * Math.cos(eq[1] * (x + eq[2]))) + eq[3]
+            }
+            for (let xindex = 0; xindex < xvalues.length; xindex++) {
+                yvalues.push(getCosYVal(this.eq, xvalues[xindex]) + getAdd(this.spread))
+            }
+        } else if (['tan'].includes(this.type)) { // tan functions
+            // [a, b, c, d] => f(x) = a * tan(b * x + c) + d. 0's are used for empty variables.
+            const getTanYVal = (eq, x) => {
+                if (eq.length !== 4) throw new UserInputError('Equation was not provided in proper format. [a, b, c, d] => f(x) = a * tan(b * x + c) + d. If a number is not applicable, put a 0.')
+                if (eq[0] === 0) throw new UserInputError('"a" cannot be equal to 0 in order to make a tan function')
+                if (eq[1] === 0) throw new UserInputError('"b" cannot be equal to 0 in order to make a tan function')
+
+                return (eq[0] * Math.tan(eq[1] * (x + eq[2]))) + eq[3]
+            }
+            for (let xindex = 0; xindex < xvalues.length; xindex++) {
+                yvalues.push(getTanYVal(this.eq, xvalues[xindex]) + getAdd(this.spread))
+            }
+        } else if (['asin'].includes(this.type)) { // arcsin functions
         }
 
         return {
--- api-backend/src/query-tests/generatePointsQueryTest.py
+++ api-backend/src/query-tests/generatePointsQueryTest.py
@@ -5,7 +5,7 @@ import json
 from scipy.interpolate import make_interp_spline
 
 """
-This query shows an application of the "generatePoints" query and how the request is sent and parsed.
+These queries shows applications of the "generatePoints" query and how the requests are sent and parsed. There is an example of each type of graph you can generate points for.
 """
 def polynomial():
     length = 200 # number of points returned
@@ -75,8 +75,8 @@ def log():
 
 def logistic():
     length = 100 # number of points returned
-    eq = [100, 1, 1, -2] # coefficients of the equation
-    spread = 20 # relative distance the points will be from the line
+    eq = [1000, 1, 100, -1] # coefficients of the equation
+    spread = 200 # relative distance the points will be from the line
     minX = -100 # minimum x value
     maxX = 100 # maximum x value
     graphType = 'logistic'
@@ -100,19 +100,127 @@ def logistic():
     yvals = points['yvalues']
 
     x = np.array(xvals)
+    x = np.linspace(x.min(), x.max(), length*100) # create many points to make the line as accurate as possible
     y = eq[0] / (eq[1] + (eq[2] * np.exp(eq[3] * x))) # finds y values from x values. this may give an overflow warning because the value can get very large/small
 
-    # spline = make_interp_spline(x, y) # makes graph curvy
-    # newX = np.linspace(x.min(), x.max(), 600)
-    # newY = spline(newX)
+    plt.plot(x, y) # plot line
+    plt.scatter(xvals, yvals) # plot the points
+    plt.show()
+
+def sin():
+    length = 200 # number of points returned
+    eq = [1, 1, 0, 0] # coefficients of the equation
+    spread = 0 # relative distance the points will be from the line
+    minX = 0 # minimum x value
+    maxX = 10 # maximum x value
+    graphType = 'sin'
+
+    plt.figure(figsize = (12, 6))
+    plt.title('Testing Equation Plotting: Sin Function') # labels
+    plt.xlabel('x-values')
+    plt.ylabel('y-values')
+    plt.grid(alpha =.6, linestyle ='--') # grid
+
+    """
+    Below is an example on how you can create a request with GraphQL. The example is URLEncoded but, as shown in server.js, you can also send requests in JSON. 
+    See https://graphql.org/learn/serving-over-http/ for more details.
+    """
+
+    res = json.loads(requests.get('http://localhost:3001/graphql', data={'query': f'{{ generatePoints(spread: {spread}, length: {length}, equation: {eq}, minX: {minX}, maxX: {maxX}, graphType: {graphType}) {{ xvalues yvalues }} }}'}).content.decode())
+    if(res.get('errors')): return print(res['errors']) # make sure to catch errors
+
+    points = res['data']['generatePoints']
+    xvals = points['xvalues']
+    yvals = points['yvalues']
+
+    x = np.linspace(minX, maxX*(1.1), length*50) # in order to not make line jagged, hundreds to thousands of points are needed
+    y = (eq[0] * np.sin(eq[1] * (x + eq[2]))) + eq[3] # finds y values from x values
+
+    plt.plot(x, y) # plot line
+    plt.scatter(xvals, yvals) # plot the points
+    plt.show()
+
+def cos():
+    length = 200 # number of points returned
+    eq = [1, 1, 0, 0] # coefficients of the equation
+    spread = 0 # relative distance the points will be from the line
+    minX = 0 # minimum x value
+    maxX = 10 # maximum x value
+    graphType = 'cos'
+
+    plt.figure(figsize = (12, 6))
+    plt.title('Testing Equation Plotting: Cos Function') # labels
+    plt.xlabel('x-values')
+    plt.ylabel('y-values')
+    plt.grid(alpha =.6, linestyle ='--') # grid
+
+    """
+    Below is an example on how you can create a request with GraphQL. The example is URLEncoded but, as shown in server.js, you can also send requests in JSON. 
+    See https://graphql.org/learn/serving-over-http/ for more details.
+    """
+
+    res = json.loads(requests.get('http://localhost:3001/graphql', data={'query': f'{{ generatePoints(spread: {spread}, length: {length}, equation: {eq}, minX: {minX}, maxX: {maxX}, graphType: {graphType}) {{ xvalues yvalues }} }}'}).content.decode())
+    if(res.get('errors')): return print(res['errors']) # make sure to catch errors
+
+    points = res['data']['generatePoints']
+    xvals = points['xvalues']
+    yvals = points['yvalues']
+
+    x = np.linspace(minX, maxX*(1.1), length*50) # in order to not make line jagged, hundreds to thousands of points are needed
+    y = (eq[0] * np.cos(eq[1] * (x + eq[2]))) + eq[3] # finds y values from x values
+
+    plt.plot(x, y) # plot line
+    plt.scatter(xvals, yvals) # plot the points
+    plt.show()
+
+def tan(): # this might return less points than you want because some points could have y values in the thousands
+    length = 100 # number of points returned
+    eq = [1, 1, 0, 0] # coefficients of the equation
+    spread = 0 # relative distance the points will be from the line
+    minX = 0 # minimum x value
+    maxX = 30 # maximum x value
+    graphType = 'tan'
+
+    plt.figure(figsize = (12, 6))
+    plt.title('Testing Equation Plotting: Tan Function') # labels
+    plt.xlabel('x-values')
+    plt.ylabel('y-values')
+    plt.grid(alpha =.6, linestyle ='--') # grid
+
+    plt.ylim(-200, 200) # limits to the x and y axis are needed to properly show the graph
+    plt.xlim(minX, maxX)
+
+    """
+    Below is an example on how you can create a request with GraphQL. The example is URLEncoded but, as shown in server.js, you can also send requests in JSON. 
+    See https://graphql.org/learn/serving-over-http/ for more details.
+    """
+
+    res = json.loads(requests.get('http://localhost:3001/graphql', data={'query': f'{{ generatePoints(spread: {spread}, length: {length}, equation: {eq}, minX: {minX}, maxX: {maxX}, graphType: {graphType}) {{ xvalues yvalues }} }}'}).content.decode())
+    if(res.get('errors')): return print(res['errors']) # make sure to catch errors
+
+    points = res['data']['generatePoints']
+    xvals = points['xvalues']
+    yvals = points['yvalues']
+
+    x = np.linspace(minX, maxX*(1.1), length*100000) # in order to not make line jagged, hundreds to thousands of points are needed
+    y = (eq[0] * np.tan(eq[1] * (x + eq[2]))) + eq[3] # finds y values from x values
+
+    plt.plot(x, y) # plot line
+    plt.scatter(xvals, yvals) # plot the points
+    plt.show()
+
 
     plt.plot(x, y) # plot line. this line will look rigid, but you can use the commented code above to make it curvy; however, the commented code above will not make it in the shape of a logistic function properly
     plt.scatter(xvals, yvals) # plot the points
     plt.show()
 
-# more examples will be added soon!
+#  more examples will be added soon!
 
 if __name__ == '__main__':
     # polynomial()
-    log()
+    # log()
     # logistic()
+    # sin()
+    # cos()
+    # tan()
+    asin()
